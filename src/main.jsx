f6290a679c16d04c064ec5322d64427b3944524d
Lokeshsharmaa-01 <iamlokesh2024@gmail.com>
2024-09-29 18:46:22 +0530

    learn more

--- src/main.jsx
+++ src/main.jsx
@@ -8,43 +8,43 @@ import Home from './Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contect/Contect.jsx'
 import User from './components/user/User.jsx'
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <Layout />,
-//     children: [
-//       {
-//         path: "",
-//         element: <Home />
-//       },
-//       {
-//         path: "about",
-//         element: <About />
-//       },
-//       {
-//         path: "contect",
-//         element: <Contact />
-//       }
-//     ]
-//   }
-// ])
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "contect",
+        element: <Contact />
+      }
+    ]
+  }
+])
 
-const routeer = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='contect' element={<Contact />} />
-      <Route path='user/:userid' element={<User />} />
+// const routeer = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path='/' element={<Layout />}>
+//       <Route path='' element={<Home />} />
+//       <Route path='about' element={<About />} />
+//       <Route path='contect' element={<Contact />} />
+//       <Route path='user/:userid' element={<User />} />
 
 
-    </Route>
-  )
-)
+//     </Route>
+//   )
+// )
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={routeer} />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
